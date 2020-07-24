1b4a73aaf03c15d32cfb0651da39c7930a1b813c
shutt <ricardo@willbe.co>
2020-07-24 17:00:13 +0100

    blabla

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const routes = require('./routes');
+require("dotenv/config");
+const express = require("express");
+const routes = require("./routes");
 
-require('./database');
+require("./database");
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
